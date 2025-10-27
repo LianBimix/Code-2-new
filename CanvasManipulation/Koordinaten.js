23e4fd62e9f078c5bbdbd02abb0b4a60361f7cde
LianBimix <lianbimix@gmail.com>
2025-10-27 17:03:54 +0100

    finishing touches für das Koordinatensystem (nun zusammenhängend)

--- CanvasManipulation/Koordinaten.js
+++ CanvasManipulation/Koordinaten.js
@@ -12,33 +12,31 @@ var Koordinatensystem;
         crc2.beginPath();
         crc2.lineWidth = 2;
         crc2.moveTo(0, 0);
-        crc2.lineTo(0, 400);
+        crc2.lineTo(0, 600);
         crc2.moveTo(0, 0);
-        crc2.lineTo(400, 0);
+        crc2.lineTo(600, 0);
         crc2.closePath();
         crc2.stroke();
-        for (let i = 1; i < 400; i += 10) {
+        for (let i = 1; i < 600; i += 10) {
             crc2.moveTo(i, 0);
-            crc2.lineTo(i, 3);
+            crc2.lineTo(i, 5);
             crc2.stroke();
             crc2.moveTo(0, i);
-            crc2.lineTo(3, i);
+            crc2.lineTo(5, i);
             crc2.stroke();
         }
     }
     function hndtransform() {
         canvas = document.querySelector("canvas");
         crc2 = canvas.getContext("2d");
-        const translate = Math.random() * 200;
-        const scale = Math.random() * 5;
-        crc2.translate(translate, translate);
-        crc2.scale(scale, scale);
-        crc2.rotate(Math.random() * 359);
+        crc2.translate(5, 5);
+        //crc2.scale(0.75, 0.75);
+        crc2.rotate(0.5);
         const randomcolor = "#" + Math.floor(Math.random() * 16777215).toString(16);
-        crc2.fillRect(0, 0, 400, 400);
+        crc2.fillRect(0, 0, 600, 600);
         crc2.fillStyle = randomcolor;
         coordSystem();
-        crc2.resetTransform();
+        //crc2.resetTransform();
         crc2.getTransform();
     }
 })(Koordinatensystem || (Koordinatensystem = {}));
