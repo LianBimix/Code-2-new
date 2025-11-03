abde1ea4fc7e25f80eaabfdc365f90ec0cc3286b
0maumau0 <strubemaurice@gmail.com>
2025-11-03 17:55:47 +0100

    setup solarsystem

--- SolarSystem/SolarSystem.ts
+++ SolarSystem/SolarSystem.ts
@@ -1,3 +1,80 @@
 namespace SolarSystem {
-    
+    window.addEventListener("load", hndLoad);
+    export let crc2: CanvasRenderingContext2D;
+    let slider: HTMLInputElement;
+
+
+
+    function hndLoad(): void {
+        console.log("solarsystem start");
+
+        const canvas: HTMLCanvasElement | null = document.querySelector("2d");
+        if (!canvas) {
+            return;
+        }
+        slider = document.getElementById("speedvalue") as HTMLInputElement;
+
+        crc2 = <CanvasRenderingContext2D>canvas?.getContext("2d");
+        crc2.fillStyle = "#000000ff"
+
+
+        createBodies();
+
+        canvas.addEventListener("mouseup", hndClick);
+        slider.addEventListener("input", hndSlider);
+
+
+
+        window.setInterval(update, 20);
+    }
+
+    function createBodies(): void {
+        // const sun: Planet = new Planet()
+        // const ship: Ship = new Ship()
+        // const jupiter: Planet = new Planet()
+        // const iO: Planet = new Planet()
+        // const earth: Planet = new Planet()
+        // const moon: Planet = new Planet()
+        // const venus: Planet = new Planet()
+        // const saturn: Planet = new Planet()
+        // const titan: Planet = new Planet()
+        // const neptun: Planet = new Planet()
+        // const triton: Planet = new Planet()
+        // const ganymed: Planet = new Planet()
+        // const callisto: Planet = new Planet()
+        // const titania: Planet = new Planet()
+        // const rhea: Planet = new Planet()
+        // const oberon: Planet = new Planet()
+        // const lapetus: Planet = new Planet()
+        // const proteus: Planet = new Planet()
+        // const neeid: Planet = new Planet()
+        // const deinos: Planet = new Planet()
+
+    }
+
+
+    function hndSlider(): void {
+        console.log(slider.value);
+        
+
+    }
+
+    function hndClick(_event: MouseEvent): void {
+
+        // const clickPosition: Vector = (_event.clientX, _event.clientY)
+        // getMouseTarget();
+    }
+
+    function getMouseTarget(_clientPosition: Vector): void {
+
+
+    }
+    function update(): void {
+
+        crc2.fillRect(0, 0, crc2.canvas.width, crc2.canvas.height);
+        //need move objekts
+        //need draw objekts
+    }
+
+
 }
