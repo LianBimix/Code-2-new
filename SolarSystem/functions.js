9ca63859e0058d37b3a301f4efbfc8064975ddb0
giigajules <giigajules@gmail.com>
2025-11-03 16:57:36 +0100

    added functions file with randomizer function

---
+++ SolarSystem/functions.js
@@ -0,0 +1,8 @@
+"use strict";
+var solarSystem;
+(function (solarSystem) {
+    function getRandomNumber(_max, _min) {
+        return Math.floor(Math.random() * (_max - _min) + _min);
+    }
+})(solarSystem || (solarSystem = {}));
+//# sourceMappingURL=functions.js.map
