9524d8ee6eb25305b78fa5f031de0260b3dc1bbb
FetaKnight <simgleichauf@t-online.de>
2025-11-04 15:31:14 +0100

    Attempt on making Planets

--- SolarSystem/SolarSystem.js
+++ SolarSystem/SolarSystem.js
@@ -1,2 +1,18 @@
 "use strict";
+var SolarSystem;
+(function (SolarSystem) {
+    window.addEventListener("load", handleLoad);
+    SolarSystem.linewidth = 5;
+    function handleLoad(_event) {
+        console.log("Simulation start");
+        const canvas = document.querySelector("canvas");
+        if (!canvas)
+            return;
+        SolarSystem.crc2.lineWidth = SolarSystem.linewidth;
+        createPlanet;
+    }
+    function createPlanet() {
+        const planet = new SolarSystem.Planet(1, 1, Sun, SolarSystem.CELEST, 5, Hallo, Earth, 10);
+    }
+})(SolarSystem || (SolarSystem = {}));
 //# sourceMappingURL=SolarSystem.js.map
--- SolarSystem/SolarSystem.ts
+++ SolarSystem/SolarSystem.ts
@@ -1,3 +1,23 @@
 namespace SolarSystem {
-    
+    window.addEventListener("load", handleLoad);
+
+    export let crc2: CanvasRenderingContext2D;
+
+    export const linewidth: number = 5;
+
+    function handleLoad(_event: Event): void {
+
+        console.log("Simulation start");
+        const canvas: HTMLCanvasElement | null = document.querySelector("canvas");
+        if (!canvas)
+            return;
+
+        crc2.lineWidth = linewidth;
+
+        createPlanet;
+    }
+
+    function createPlanet(): void {
+        const planet: Planet = new Planet(1, 1, Sun, CELEST, 5, Hallo, Earth, 10);
+    }
 }
